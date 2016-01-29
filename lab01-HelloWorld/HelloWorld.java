30d94d7067f06f000b34b8e4f089e074768c64ca
reillycallahan <rec219@lehigh.edu>
2016-01-29 15:54:42 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,30 @@
+/////////////////
+//Reilly Callahan
+//Jan 29, 2016
+//Lab01
+//Hello World Java Program
+//
+//First, compile the program
+//     javac HelloWorld.java
+//then, run the program
+//      java HelloWorld//
+
+//define a class
+public class HelloWorld{
+    
+//add main method
+    public static void main(String[] args) {
+        
+//print the statement with box around it
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("#                      #");
+    System.out.println("#  Hello World 2016!!  #");
+    System.out.println("#                      #");
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("test");
+    
+    //test/testtest
+    }
+}
